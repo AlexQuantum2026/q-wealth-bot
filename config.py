e642f2d55c3c3e2f250beabc0aaca804642d1b4a
Schwarz121 <feketesandor0316@gmail.com>
2026-02-05 22:06:35 +0100

    Initial config setup v1.0.4

---
+++ config.py
@@ -0,0 +1,7 @@
+# Q-Wealth Bot Configuration v1.0.4
+API_ENDPOINT = "https://vip.q-wealth.com/api"
+
+# SENSITIVE DATA - DO NOT COMMIT
+ADMIN_EMAIL = "alex@q-wealth.com"
+DEV_EMAIL = "bela.dev@freemail.hu"
+BACKUP_EMAIL = "money_mule_2026@proton.me"
